00d4b9b86c0aef142c31b2a730e8fced03ba9196
600020344 <77069279+600020344@users.noreply.github.com>
2021-04-16 18:44:51 -0700

    Update SyntaxChecker.java

--- SyntaxChecker.java
+++ SyntaxChecker.java
@@ -13,20 +13,71 @@ public class SyntaxChecker
 
  public SyntaxChecker()
  {
+   exp = "";
+   symbols = new Stack<Character>();
  }
 
  public SyntaxChecker(String s)
  {
+   exp = s;
+   symbols = new Stack<Character>();
  }
  
  public void setExpression(String s)
  {
+   exp = s;
+   symbols = new Stack<Character>();
  }
 
  public boolean checkExpression()
  {
-  return false;
+  for (int i = 0; i<exp.length(); i++)
+  {
+    char c = exp.charAt(i);
+    // System.out.println("c is " + c);
+    switch (c)
+    {
+      case '{':
+      case '(':
+      case '<':
+      case '[':
+        symbols.push(c);
+        // System.out.println("pushed " + c);
+        break;
+      case '}':
+      case ')':
+      case '>':   
+      case ']':
+        if(!symbols.isEmpty())
+        {
+          char popped = symbols.pop();
+          // System.out.println("popped " + popped);
+          if ((popped == '}' && c!='{') ||(popped == ')' && c!='(') || (popped == '>' && c!='<')||(popped == ']' && c!='['))
+            return false;
+        } else {
+          return false;
+        }
+        break;
+      default: break;
+    }
+  }
+  if(!symbols.isEmpty())
+  {
+    return false;
+  }
+      return true;
  }
 
  //write a toString
+ public String toString()
+ {
+   String s = exp + " is ";
+   if(checkExpression())
+   {
+     s+= "correct.";
+   } else {
+     s+= "incorrect.";
+   }
+   return s;
+ }
 }
