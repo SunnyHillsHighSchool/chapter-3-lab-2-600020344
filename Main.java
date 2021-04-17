1b8077fec99de364f70da9525f6402262239533b
600020344 <77069279+600020344@users.noreply.github.com>
2021-04-16 18:45:58 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,5 +10,24 @@ import static java.lang.System.*;
 class Main {
   public static void main(String[] args) {
     //add test cases  
+    SyntaxChecker sc = new SyntaxChecker("(abc(*def)");
+    System.out.println(sc);
+    sc.setExpression("[{}]");
+    System.out.println(sc);
+    sc.setExpression("[");
+    System.out.println(sc);
+    sc.setExpression("[{<()>}]");
+    System.out.println(sc);
+    sc.setExpression("{<html[value=4]*(12)>{$x}}");
+    System.out.println(sc);
+    sc.setExpression("[one]<two>{three}(four)");
+    System.out.println(sc);
+    sc.setExpression("car(cdr(a)(b)))");
+    System.out.println(sc);
+    sc.setExpression("car(cdr(a)(b))");
+    System.out.println(sc);
+    
+    
+    
   }
 }
